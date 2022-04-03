da63179f70ca38699a36b5d74759f12eb5bf45f9
GeonwooVincentKim <kdsnop@gmail.com>
2022-04-03 21:47:33 +0900

    [Feature] - Added Mixins.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+// import store from './store'
+import mixins from "./mixins";
 
-createApp(App).use(store).use(router).mount('#app')
+
+createApp(App).use(router).mixin(mixins).mount('#app')
