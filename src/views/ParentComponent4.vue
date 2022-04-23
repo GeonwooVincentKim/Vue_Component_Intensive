daa0f7e4b494cab7e14ebaeb173db7eee20c0c7b
GeonwooVincentKim <kdsnop@gmail.com>
2022-04-23 09:38:12 +0000

    [Feature] - Added Parent COmponents

---
+++ src/views/ParentComponent4.vue
@@ -0,0 +1,15 @@
+<template>
+	<child-component-4 @send-message="sendMessage" />
+</template>
+<script>
+import ChildComponent4 from './ChildComponent4.vue'
+
+export default {
+	components: {ChildComponent4},
+	methods : {
+		sendMessage(data) {
+			console.log(data);
+		}
+	}
+}
+</script>
