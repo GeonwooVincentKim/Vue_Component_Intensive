f273a48a6eb1031a759c10259208142c2c5e83c7
GeonwooVincentKim <kdsnop@gmail.com>
2022-05-12 10:40:15 +0900

    [Feature] - Composition API3

---
+++ src/views/calculator/CompositionAPI3.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <input type="text" v-model="num1">
+        <span> + </span>
+        <input type="text" v-model="num2">
+        <span> = </span>
+        <span>{{ result }}</span>
+    </div>
+</template>
+<script>
+import { reactive, computed, toRefs } from "vue";
+
+function plusCalculator() {
+  let state = reactive({
+    num1: 0,
+    num2: 0,
+    result: computed(() => parseInt(state.num1) + parseInt(stsate.num2))
+  })
+
+  // Use `toRefs` to make sure `num1, num2, result` works normally 
+  // at External-Function
+  return toRefs(state)
+}
+
+export default {
+  setup () {
+    let {num1, num2, result} = plusCalculator() // External Function
+    return {
+      num1, num2, result
+    }      
+  },
+}
+</script>
