78cb761cb45666b5c304eb020dd37a95c2a451c2
GeonwooVincentKim <kdsnop@gmail.com>
2022-05-13 09:26:44 +0900

    [Feature] - Added Mixins.vue file

---
+++ src/views/mixins/Mixins.vue
@@ -0,0 +1,19 @@
+<template>
+    <div></div>
+</template>
+<script>
+import ApiMixin from '../api.js'
+
+export default {
+  mixins: [ApiMixin], // Add Mixin File to use into the Array
+  data () {
+    return {
+      productList: []
+    }
+  },
+  async mounted () {
+    this.productList = await this.$callAPI("https://ada1e106-f1b6-4ff2-be04-e311ecba599d.mock.pstmn.io/list", "get")
+    console.log(this.productList)
+  }
+}
+</script>
