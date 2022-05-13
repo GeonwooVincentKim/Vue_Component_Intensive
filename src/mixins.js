6a455c1865612207c6ac63687cda141472c93916
GeonwooVincentKim <kdsnop@gmail.com>
2022-05-13 10:22:01 +0900

    [Update] - Check current Mixins mouted or unmounted

--- src/mixins.js
+++ src/mixins.js
@@ -1,6 +1,14 @@
 import axios from 'axios'
 
 export default {
+  mounted () {
+    console.log('Mixins Mounted')
+  },
+
+  unmounted () {
+    console.log('Mixins Unmounted')
+  },
+
   methods: {
     async $api (url, method, data) {
       return (await axios({
