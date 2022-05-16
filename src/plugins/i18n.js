1315cb3a0684c9fa5fe68f98dd7e5a2f81711b49
GeonwooVincentKim <kdsnop@gmail.com>
2022-05-16 13:20:06 +0900

    [Update] - Updated CustomDirective.vue page

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,10 +1,10 @@
-export default {
-  install: (app, options) => {
-    app.config.globalProperties.$translate = key => {
-      return key.split('.').reduce((o, i) => {
-        if (o) return o[i]
-      }, options)
-    }
-    app.provide('i18n', options)
-  }
-}
+// export default {
+//   install: (app, options) => {
+//     app.config.globalProperties.$translate = key => {
+//       return key.split('.').reduce((o, i) => {
+//         if (o) return o[i]
+//       }, options)
+//     }
+//     app.provide('i18n', options)
+//   }
+// }
--- src/views/mixins/CustomDirective.vue
+++ src/views/mixins/CustomDirective.vue
@@ -8,6 +8,18 @@ export default {
       mounted (el) {
         el.focus()
       }
+    },
+    pin: {
+      mounted (el, binding) {
+        el.style.position = 'fixed'
+        el.style.top = binding.value.top + 'px'
+        el.style.left = binding.value.left + 'px'
+      }
+    }
+  },
+  data () {
+    return {
+      position: { top: 50, left: 100 }
     }
   }
 }
