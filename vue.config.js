5cc226e189818b341e5a8f6f16224aaa2b1317c5
GeonwooVincentKim <kdsnop@gmail.com>
2022-05-17 21:16:41 +0900

    [Feature] - Added vue.config.js file

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,18 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+const target = 'http://127.0.0.1:3000'
+
+module.exports = {
+  devServer: {
+    port: 8000,
+    proxy: {
+	  // Start point of API sends `Proxy` request
+      '^/api': {
+        target,
+		changeOrigin: true
+	  }
+	}
+  }
+}
