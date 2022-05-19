fb2abefc3afdfd4f350c213f96e25dd423a8887b
GeonwooVincentKim <kdsnop@gmail.com>
2022-05-19 13:14:08 +0900

    [Feature] - Added StoreAccess.vue file

--- src/views/store/StoreAccess.vue
+++ src/views/store/StoreAccess.vue
@@ -5,18 +5,20 @@
 </template>
 <script>
 export default {
-  // Getters
+  // Manage State
   computed: {
     count () {
       console.log('Check Store Count (Vue) -> ', this.$store.state.count)
       return this.$store.state.count
     },
+    // Can access to the storage defined value by `getters`
+    // Getters
     cartCount () {
       console.log('Check Cart Count (Vue) -> ', this.$store.getters.cartCount)
       return this.$store.getters.cartCount
     }
   },
-  // Mutations
+  // Mutations - Does not allows to change defined variable
   methods: {
     increment () {
       console.log('Check Clicked -> ', this.count)
