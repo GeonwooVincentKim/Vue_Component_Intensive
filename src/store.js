6f4bc9cb61d88c1f3f0e5fdc8e162401de06bb4e
GeonwooVincentKim <kdsnop@gmail.com>
2022-05-19 11:21:40 +0900

    [Update] - Updated Store.js file

--- src/store.js
+++ src/store.js
@@ -3,7 +3,17 @@ import { createStore } from 'vuex'
 const store = createStore({
   state () {
     return {
-      count: 0
+      count: 0,
+      cart: [{
+        product_id: 1,
+        product_name: '아이폰 거치대',
+        category: 'A'
+      }]
+    }
+  },
+  getters: {
+    cartCount: (state) => {
+      return state.cart.length
     }
   },
   mutations: {
