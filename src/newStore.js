44b67f33a18d57a32835f5217e3d2f49e1b72f63
GeonwooVincentKim <kdsnop@gmail.com>
2022-05-20 09:57:03 +0900

    [Fix & Update] - vuex unknown mutation type increment

--- src/newStore.js
+++ src/newStore.js
@@ -1,22 +1,22 @@
-import { createStore } from 'vuex'
-import persistedstate from 'vuex-persistedstate'
+// import { createStore } from 'vuex'
+// import persistedstate from 'vuex-persistedstate'
 
-const newStore = createStore({
-  state () {
-    return {
-      user: {}
-    }
-  },
-  mutations: {
-    user (state, data) {
-      state.euser = data
-    }
-  },
-  plugins: [
-    persistedstate({
-      path: ['user']
-    })
-  ]
-})
+// const newStore = createStore({
+//   state () {
+//     return {
+//       user: {}
+//     }
+//   },
+//   mutations: {
+//     user (state, data) {
+//       state.euser = data
+//     }
+//   },
+//   plugins: [
+//     persistedstate({
+//       path: ['user']
+//     })
+//   ]
+// })
 
-export default newStore
+// export default newStore
