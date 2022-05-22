2bcf7bda1163fd37c2f43d066aed76846891b33d
GeonwooVincentKim <kdsnop@gmail.com>
2022-05-22 20:36:23 +0900

    [Feature] - Added Naver

---
+++ src/views/naver/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div id="naverIdLogin"></div>
+    <button type="button" @click="logout">LogOut</button>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  data () {
+    return {
+      naverLogin: null
+    }
+  },
+  mounted () {
+    this.naverLogin = new window.naver.LoginWithNaverId({
+      clientId: 'kdsnop',
+      callbackUrl: 'http://localhost:8080/naverlogin',
+      isPopup: false, // 
+      loginButton: {
+        color: 'green', type: 3, height: 60
+      }
+    })
+    this.naverLogin.init()
+
+  }
+}
+</script>
