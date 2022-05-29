d4290e0db89b6032c7027971e04252f7c6cd886d
GeonwooVincentKim <kdsnop@gmail.com>
2022-05-29 21:46:48 +0900

    [Update] - Updated GoogleLogin

--- src/views/google/GoogleLogin.vue
+++ src/views/google/GoogleLogin.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
     <div>
         <div id="google-signin-btn"></div>
     </div>
@@ -6,11 +6,18 @@
 <script>
 export default {
   mounted () {
-    window.gapi.signin2.render("google-signin-btn", {onsuccess: this.onSignIn})
+    // window.gapi.signin2.render("google-signin-btn", {onsuccess: this.onSignIn})
+	window.gapi.load('client:auth2', () => {
+		window.gapi.client.init({
+			clientId: '1031320944798-ag9d52lnklg0eucemgaemjtdnpt9cg80.apps.googleusercontent.com',
+			plugin_name: 'VueClient'
+		})
+	})
+	console.log('CHheck Mounted ?? ')
   },
-  
   methods: {
     onSignIn (googleUser) {
+	  console.log('Check Here -> ')
       const profile = googleUser.getBasicProfile()
       console.log(profile)
 
@@ -25,5 +32,43 @@ export default {
       console.log("ID Token -> " + id_token)
     }
   }
+}
+</script> -->
+<template>
+	<g-signin-button
+		:params="googleSignInParams"
+		@success="onSignInSuccess"
+		@error="onSignInError">
+		Sign in with Google
+	</g-signin-button>
+</template>
+<script>
+export default {
+	data() {
+		return {
+			googleSignInParams: {
+				clientId: '1031320944798-ag9d52lnklg0eucemgaemjtdnpt9cg80.apps.googleusercontent.com'
+			}
+		}
+	},
+	methods: {
+		onSignInSuccess (googleUser) {
+			const profile = googleUser.getBasicProfile()
+			console.log(profile)
+
+			console.log("ID -> " + profile.getId())
+			console.log("Full Name -> " + profile.getName())
+			console.log("Given Name -> " + profile.getGivenName())
+			console.log("Family Name -> " + profile.getFamilyName())
+			console.log("Image URL -> " + profile.getImageUrl())
+			console.log("Email -> " + profile.getEmail())
+
+			const id_token = googleUser.getAuthResponse().id_token
+			console.log("ID Token -> " + id_token)
+		},
+		onSignInError (error) {
+			console.log('OH NOES', error)
+		}
+	}
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -18,18 +18,18 @@
 //   }
 // }
 
-module.exports = {
-  chainWebpack: config => {
-    config.plugins.delete('prefetch')
-  },
-  devServer: {
-    proxy: {
-      '/oauth2.0': {
-        target: 'https://nid.naver.com'
-      }
-    }
-  },
-}
+// module.exports = {
+//   chainWebpack: config => {
+//     config.plugins.delete('prefetch')
+//   },
+//   devServer: {
+//     proxy: {
+//       '/oauth2.0': {
+//         target: 'https://nid.naver.com'
+//       }
+//     }
+//   },
+// }
 
 module.exports = {
   lintOnSave: false
